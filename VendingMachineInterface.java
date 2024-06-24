ad2b8a3d80c36b8e7703ad83b7c9c84e98f9c776
Julian Lunger <jdlunger@users.noreply.github.com>
2024-06-24 09:30:38 +0000

    Extend example a bit with a `SodaFetcher`.

--- VendingMachineInterface.java
+++ VendingMachineInterface.java
@@ -1,27 +1,45 @@
-public class Main {
+class Main {
     public static void main(String[] args) {
-        VendingMachine machine1 = new AutoMachine();
-        VendingMachine machine2 = new PersonMachine();
+        SodaFetcher fetcher1 = new SodaFetcher(new AutoMachine());
+        SodaFetcher fetcher2 = new SodaFetcher(new PersonMachine());
 
-        machine1.getCoke();
-        machine2.getCoke();
+        fetcher1.fetchSoda(5);
+        fetcher2.fetchSoda(5);
+        fetcher2.fetchSoda(1);
     }
 }
 
-interface VendingMachine {
+class SodaFetcher {
+    IVendingMachine machine;
+
+    SodaFetcher(IVendingMachine machine) {
+        this.machine = machine;
+    }
+
+    void fetchSoda(int money) {
+        if (money > 3) {
+            System.out.println("Got enough money; fetching soda.");
+            machine.getCoke();
+        } else {
+            System.out.println("Not enough money; not fetching soda.");
+        }
+    }
+}
+
+interface IVendingMachine {
     void getCoke();
 }
 
-class AutoMachine implements VendingMachine {
+class AutoMachine implements IVendingMachine {
     @Override
     public void getCoke() {
-        System.out.println("Vending Coke automatically");
+        System.out.println("> Vending Coke automatically.");
     }
 }
 
-class PersonMachine implements VendingMachine {
+class PersonMachine implements IVendingMachine {
     @Override
     public void getCoke() {
-        System.out.println("Small person giving you a coke");
+        System.out.println("> Small person giving you a Coke.");
     }
 }
